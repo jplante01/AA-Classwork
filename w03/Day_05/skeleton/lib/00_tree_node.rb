717f40a92ad86b25c1e2f4b4f34fa350e3a71f0d
user <jeffdplante@aol.com>
2021-09-20 11:58:29 -0700

    finished initialize

--- w03/Day_05/skeleton/lib/00_tree_node.rb
+++ w03/Day_05/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,19 @@
 class PolyTreeNode
-# sdfs
+  attr_reader :children, :value, :parent
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+  
+  def parent=(other_node)
+    @parent = other_node
+    other_node.children << self
+
+    
+  end
+
 end
+
+#set parent on instance
+#pass in the parent object
