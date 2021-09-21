060d3c5a66305ffd2a4f82d58d5f2aad274301a0
user <jeffdplante@aol.com>
2021-09-21 10:26:29 -0700

    created chess folder

--- w03/d05/skeleton/lib/00_tree_node.rb
+++ w03/d05/skeleton/lib/00_tree_node.rb
@@ -1,19 +1,58 @@
+require "byebug"
 class PolyTreeNode
-  attr_reader :children, :value, :parent
+  attr_accessor :children, :value, :parent
+  
   def initialize(value)
     @parent = nil
     @children = []
     @value = value
   end
   
-  def parent=(other_node)
-    @parent = other_node
-    other_node.children << self
-
-    
+  def inspect
+    { 'val' => @value}.inspect
   end
 
-end
+  def parent=(other_node)
+    if @parent.nil?
+      @parent = other_node
+      other_node.children << self
+    elsif other_node == nil
+      @parent = nil
+    else
+      @parent.children.delete(self)
+      @parent = other_node
+      other_node.children << self
+    end
+  end
 
-#set parent on instance
-#pass in the parent object
+  def add_child(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    child.parent = nil
+  end
+
+  def dfs(target_value)
+
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    # debugger
+    until queue.empty?
+      queue_head = queue[0]
+      return queue_head if queue_head.value == target_value
+      queue.shift
+      if queue_head.children.empty? == false
+        queue_head.children.each do |child|
+        queue << child
+        end
+      end
+    end
+    nil
+  end
+    
+ 
+
+end
