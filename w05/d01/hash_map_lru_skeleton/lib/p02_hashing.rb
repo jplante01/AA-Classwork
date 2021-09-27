b1121497cf2eb0cc566676cf0cdc6d6f0b89d437
Julian Erville <jerville@gmail.com>
2021-09-27 19:42:43 -0400

    Add hash functions

--- w05/d01/hash_map_lru_skeleton/lib/p02_hashing.rb
+++ w05/d01/hash_map_lru_skeleton/lib/p02_hashing.rb
@@ -4,12 +4,21 @@ end
 
 class Array
   def hash
-    self.
+    new_str = ""
+    self.each.with_index do |el, i|
+      new_str += el.to_s + i.to_s
+    end
+    new_str.to_i ^ 9999
   end
 end
 
 class String
   def hash
+    new_str = ""
+    self.each_char do |char|
+      new_str += char.ord.to_s 
+    end
+    new_str.to_i ^ 9999
   end
 end
 
@@ -17,6 +26,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    self.to_
+    p 0
   end
 end
