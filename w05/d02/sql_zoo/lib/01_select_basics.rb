7cba6e9b8e0a172d66b5de5fcf9b9ad932d0b2d5
user <jeffdplante@aol.com>
2021-09-28 14:56:55 -0700

    got started

--- w05/d02/sql_zoo/lib/01_select_basics.rb
+++ w05/d02/sql_zoo/lib/01_select_basics.rb
@@ -36,6 +36,14 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
+  
+  SELECT 
+    name, gdp/population
+  FROM
+    countries
+  WHERE
+    area > 5000000;
+
   SQL
 end
 
@@ -43,6 +51,13 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
+
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    area < 2000 AND gdp > 5000000000;
   SQL
 end
 
