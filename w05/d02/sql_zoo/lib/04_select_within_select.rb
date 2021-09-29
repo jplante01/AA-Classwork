a76fd48584d5dc30418736b4503eab6465534e78
user <jeffdplante@aol.com>
2021-09-28 17:34:36 -0700

    we done

--- w05/d02/sql_zoo/lib/04_select_within_select.rb
+++ w05/d02/sql_zoo/lib/04_select_within_select.rb
@@ -97,6 +97,30 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+    name, population
+  FROM
+    countries
+  WHERE
+    population < (
+      SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Poland'
+    ) 
+
+    AND population > (
+      SELECT
+        population
+      FROM
+        countries
+      WHERE
+        name = 'Canada' 
+    );
+
+
   SQL
 end
 
@@ -106,5 +130,18 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+    name, continent, population
+  FROM
+    countries
+  WHERE 
+    continent NOT IN  
+      (SELECT 
+        continent
+      FROM
+        countries
+      WHERE
+        population > 25000000);
+ 
   SQL
 end
