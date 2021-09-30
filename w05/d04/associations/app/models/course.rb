8c3632d406f3a4a73852878fbfb51b4dc795021f
user <jeffdplante@aol.com>
2021-09-30 16:14:20 -0700

    asdf

--- w05/d04/associations/app/models/course.rb
+++ w05/d04/associations/app/models/course.rb
@@ -1,2 +1,43 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
 end
+
+# class Strike < ApplicationRecord
+#   belongs_to :student,
+#   	primary_key: :id,
+#   	foreign_key: :student_id,
+#   	class_name: :Student
+# end
+
+# def belongs_to(:name, options = {})
+# end
+
+# class Post < ApplicationRecord
+#   #validations go here
+#   belongs_to :user,
+#     primary_key: :id,
+#     foreign_key: :author_id,
+#     class_name: :User
+# end
+
+# class User < ApplicationRecord
+#   #validations go here
+#   has_many :posts,
+#     primary_key: :id,
+#     foreign_key: :author_id,
+#     class_name: :Post
+# end
--- w05/d04/associations/app/models/enrollment.rb
+++ w05/d04/associations/app/models/enrollment.rb
@@ -1,2 +1,44 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+  belongs_to :courses,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+
 end
+
+
+# class Strike < ApplicationRecord
+#   belongs_to :student,
+#   	primary_key: :id,
+#   	foreign_key: :student_id,
+#   	class_name: :Student
+# end
+
+# def belongs_to(:name, options = {})
+# end
+
+# class Post < ApplicationRecord
+#   #validations go here
+#   belongs_to :user,
+#     primary_key: :id,
+#     foreign_key: :author_id,
+#     class_name: :User
+# end
+
+# class User < ApplicationRecord
+#   #validations go here
+#   has_many :posts,
+#     primary_key: :id,
+#     foreign_key: :author_id,
+#     class_name: :Post
+# end
