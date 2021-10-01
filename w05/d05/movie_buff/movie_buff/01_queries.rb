be3c00c241a6ef3795e238f4217441c721d9ad2b
Matthew Lese <matthewjlese@gmail.com>
2021-10-01 14:19:29 -0700

    completes biggest_cast

--- w05/d05/movie_buff/movie_buff/01_queries.rb
+++ w05/d05/movie_buff/movie_buff/01_queries.rb
@@ -22,10 +22,16 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
 
-  Actor
-      .joins(:movies)
-      .where("name = 'Harrison Ford' AND ord != 1")
-      .select(:movie_id, :title)
+  Movie
+    .joins(:actors)
+    .where(actors: {name: 'Harrison Ford'})
+    .where.not(castings: {ord: 1})
+    .select(:id, :title)
+
+#   Actor
+#       .joins(:movies)
+#       .where("name = 'Harrison Ford' AND ord != 1")
+#       .select(:movie_id, :title)
 end
 
 def biggest_cast
@@ -42,6 +48,14 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+
+  Movie
+    .joins(:castings)
+    .group(:id)
+    .order('COUNT(actor_id) DESC')
+    .limit(3)
+    .select(:id, :title)
+
 
 end
 
