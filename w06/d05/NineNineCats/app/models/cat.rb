725238e3395c82770b2a1652ec1c7ef703e668d7
Jan Daniel Fermin <jandanielfermin@gmail.com>
2021-10-08 15:28:18 -0700

    Finished age

--- w06/d05/NineNineCats/app/models/cat.rb
+++ w06/d05/NineNineCats/app/models/cat.rb
@@ -1,2 +1,14 @@
+require "action_view"
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  validates :sex, inclusion: {in: ['M', 'F']}
+  validates :birth_date, :colors, :name, :sex, presence: true
+
+  def age
+    today = Date.today
+    age = today.year - self.birth_date.year
+    age += 1 if today.yday < self.birth_date.yday 
+    return age
+  end
 end
--- w06/d05/NineNineCats/db/migrate/20211008212658_create_cats.rb
+++ w06/d05/NineNineCats/db/migrate/20211008212658_create_cats.rb
@@ -1,12 +1,12 @@
 class CreateCats < ActiveRecord::Migration[5.2]
   def change
-    validates :sex, limit: 1
+    # validates :sex, limit: 1
 
     create_table :cats do |t|
       t.date :birth_date, null: false
       t.string :colors, null: false
       t.string :name, null: false
-      t.string :sex, null: false
+      t.string :sex, null: false, limit: 1
       t.text :description
 
       t.timestamps
