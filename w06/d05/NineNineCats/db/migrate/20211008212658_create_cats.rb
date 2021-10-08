fc7286f17b90b404e26e9c5f63aa89aa4258ccf9
user <jeffdplante@aol.com>
2021-10-08 14:42:26 -0700

    nineninecats started

---
+++ w06/d05/NineNineCats/db/migrate/20211008212658_create_cats.rb
@@ -0,0 +1,16 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+    validates :sex, limit: 1
+
+    create_table :cats do |t|
+      t.date :birth_date, null: false
+      t.string :colors, null: false
+      t.string :name, null: false
+      t.string :sex, null: false
+      t.text :description
+
+      t.timestamps
+    end
+    add_index :cats, :name
+  end
+end
